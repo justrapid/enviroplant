8e90b43a3ddd4ff53087218b706168a74d4b9e16
justrapid <sampaoletta@gmail.com>
2025-10-30 22:09:15 -0400

    Changing the temperature variable to show in Fahrenheit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from bme280 import BME280
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
-temperature =  bme280.get_temperature()
+temperature =  (bme280.get_temperature() * 1.8) + 32
 print(temperature)
