816023d75b3fe386c751127cffd7fc66ad72ab84
justrapid <sampaoletta@gmail.com>
2025-12-01 18:13:48 -0500

    Testing the framework for changing image based on sensors, and adding light functionality.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#from smbus2 import SMBus
-#from bme280 import BME280
+from smbus2 import SMBus
+from bme280 import bme280
 import cv2 as cv
 import sys
 import math
+from ltr559 import LTR559
+import time
 
-#bus = SMBus(1)
-#bme280 = BME280(i2c_dev=bus)
+
+bus = SMBus(1)
+ltr559 = LTR559()
+bme280 = bme280(i2c_dev=bus)
 min_temp = 0
 max_temp = 0
 min_lux = 0
@@ -48,18 +52,25 @@ elif plant == "tomato":
     max_lux = 10000
     max_temp = 85
 
-#temperature = (bme280.get_temperature() * 1.8) + 32 #Formula to change temp to F. Will put this in a loop eventually.
-light = "in progress"
+temperature = (bme280.get_temperature() * 1.8) + 32 #Formula to change temp to F. Will put this in a loop eventually.
+lux = (ltr559.get_lux() * 4) #Should be CLOSE to the true lux on the sensor.
 key = None
 img = cv.imread("images\\blank.png")
 cv.imshow("Plant Mood", img)
 key = cv.waitKey(0)
 
-while key != ord("e"):
-    key = cv.waitKey(0)
-    #temperature = (bme280.get_temperature() * 1.8) + 32
-    if plant == "tomato":
-        #if min_temp <= temperature <= max_temp:
+if plant == "tomato":
+    while key != ord("e"):
+        key = cv.waitKey(0)
+        temperature = (bme280.get_temperature() * 1.8) + 32
+        lux = (ltr559.get_lux() * 4)
+        if min_temp <= temperature <= max_temp and min_lux <= lux <= max_lux:
             img = cv.imread("images\happy_tomato.png")
             cv.imshow("Plant Mood", img)
-            
+            time.sleep(1.0)
+        else:
+            img = cv.imread("images\sad_tomato.png")
+            cv.imshow("Plant Mood", img)
+            time.sleep(1.0)
+            print(f"Light = {lux} and temperature = {temperature}")
+                
